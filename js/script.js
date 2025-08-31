86782993bfcd9411ec69bf79a46a1c0a1296ed86
ChandraVamsi06 <chandravamsisana06@gmail.com>
2025-08-31 20:07:27 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function Close() {
+    document.getElementsByClassName('mobile-option')[0].style = "right: -200px;"
+}
+function Open() {
+    document.getElementsByClassName('mobile-option')[0].style = "right: 0px;"
+}
