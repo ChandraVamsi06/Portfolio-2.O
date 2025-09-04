55fdf00a7b36a1908f7d3d03aeb6248dc8a197e6
ChandraVamsi06 <chandravamsisana06@gmail.com>
2025-09-04 23:05:48 +0530

    fourth commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ function Close() {
 function Open() {
     document.getElementsByClassName('mobile-option')[0].style = "right: 0px;"
 }
+
+function openLeetCode() {
+    window.open("https://leetcode.com/u/chandravamsi__sana", "_blank");
+}
+
+function openCodeChef() {
+    window.open("https://www.codechef.com/users/chandravamsi06", "_blank");
+}
+
+function openGeeksForGeeks() {
+    window.open("https://www.geeksforgeeks.org/user/chandravamsi__sana/", "_blank");
+}
+
+function openHackerRank() {
+    window.open("https://www.hackerrank.com/profile/chandravamsisan1", "_blank");
+}
+
+function openCodeForces() {
+    window.open("https://codeforces.com/profile/SANA_CHANDRA_VAMSI", "_blank");
+}
